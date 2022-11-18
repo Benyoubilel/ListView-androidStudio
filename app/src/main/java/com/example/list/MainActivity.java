cfce0df49ebde49d0ffc87398590d5fb36db8e64
Benyoubilel <bilel@ben-youssef.fr>
2022-11-18 13:42:03 +0100

    adding action listener

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -2,57 +2,97 @@ package com.example.list;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity<adapterView> extends AppCompatActivity {
 
-    AdapterView.OnItemClickListener listener1 = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            selected.setText(items.get(i));
-
-            Toast.makeText(MainActivity.this, String.valueOf(i), Toast.LENGTH_LONG).show();
-        }
-    };
-
-    ArrayList<String> items;
-    ListView liste;
-    ArrayAdapter aa;
-    TextView selected;
+    ArrayList<String> items = new ArrayList<String>();
+    ListView listeView1;
+    ArrayAdapter myAdapter;
+    EditText inputText1;
+    Integer indexVal;
+    String item;
+    Button btnAdd,btnUpdate,btnDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        selected = findViewById(R.id.selectedItem);
-        items = new ArrayList<>();
-        liste = findViewById(R.id.list);
-        aa = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                items);
-        liste.setAdapter(aa);
-        liste.setOnItemClickListener(listener1);
-        items.add("BMW");
-    }
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnUpdate = findViewById(R.id.buttonUpdate);
+        btnDelete = findViewById(R.id.buttonDelete);
+        inputText1 = findViewById(R.id.edit_text);
+        listeView1 = findViewById(R.id.list);
+        items.add("bilel");
+        items.add("hamma");
 
-    public void buttonClick(View view) {
-        items.add("KAWAZAKI");
-        aa.notifyDataSetChanged();
+        myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listeView1.setAdapter(myAdapter);
 
-    }
-    public void buttonDelClick(){
+        // add items
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String val = inputText1.getText().toString();
+                if (!val.isEmpty()) {
+                    items.add(val);
+                    myAdapter.notifyDataSetChanged();
+                    inputText1.setText("");
+                    Toast.makeText(MainActivity.this, "données ajoutée", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "champs vide!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //select items
+        listeView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+                String value = adapterView.getItemAtPosition(i).toString();
+                item = adapterView.getItemAtPosition(i).toString() + " is selectes";
+                indexVal = i;
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+                inputText1.setText(value);
+            }
+        });
 
 
+        // update items
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String val = inputText1.getText().toString();
+                items.set(indexVal, val);
 
+                if (!val.isEmpty()) {
+                    myAdapter.notifyDataSetChanged();
+                    Toast.makeText(MainActivity.this, "données modifié", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "champs vide!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        // delete items
 
-
+        listeView1.setOnLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long id) {
+                item = adapterView.getItemAtPosition(i).toString() + " supprimé";
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+                items.remove(i);
+                myAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 }
