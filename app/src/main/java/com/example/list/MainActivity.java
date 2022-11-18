afaaf33caa9a599c68af8ad85b9262a6a925734b
Benyoubilel <bilel@ben-youssef.fr>
2022-11-18 21:05:38 +0100

    correction d'erreur de supression

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity<adapterView> extends AppCompatActivity {
     ListView listeView1;
     ArrayAdapter myAdapter;
     EditText inputText1;
-    Integer indexVal;
+     Integer indexVal;
     String item;
     Button btnAdd,btnUpdate,btnDelete;
     @Override
@@ -32,37 +32,47 @@ public class MainActivity<adapterView> extends AppCompatActivity {
         btnDelete = findViewById(R.id.buttonDelete);
         inputText1 = findViewById(R.id.edit_text);
         listeView1 = findViewById(R.id.list);
-        items.add("bilel");
-        items.add("hamma");
-
         myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         listeView1.setAdapter(myAdapter);
 
         // add items
-
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String val = inputText1.getText().toString();
                 if (!val.isEmpty()) {
                     items.add(val);
                     myAdapter.notifyDataSetChanged();
                     inputText1.setText("");
-                    Toast.makeText(MainActivity.this, "données ajoutée", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "data added", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "champs vide!!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "input is empty!!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        //select items
+
+        //select items and delete items
         listeView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
                 String value = adapterView.getItemAtPosition(i).toString();
                 item = adapterView.getItemAtPosition(i).toString() + " is selectes";
-                indexVal = i;
+                 indexVal = i;
                 Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
                 inputText1.setText(value);
+
+                //delete items
+
+                btnDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        items.remove(i);
+                        Toast.makeText(MainActivity.this, "data deleted", Toast.LENGTH_SHORT).show();
+                        myAdapter.notifyDataSetChanged();
+                        inputText1.setText("");
+                    }
+                });
             }
         });
 
@@ -71,27 +81,17 @@ public class MainActivity<adapterView> extends AppCompatActivity {
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String val = inputText1.getText().toString();
                 items.set(indexVal, val);
 
                 if (!val.isEmpty()) {
                     myAdapter.notifyDataSetChanged();
-                    Toast.makeText(MainActivity.this, "données modifié", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "data updated", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "champs vide!!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "input is empty!!", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-        // delete items
 
-        listeView1.setOnLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long id) {
-                item = adapterView.getItemAtPosition(i).toString() + " supprimé";
-                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
-                items.remove(i);
-                myAdapter.notifyDataSetChanged();
-                return true;
             }
         });
     }
